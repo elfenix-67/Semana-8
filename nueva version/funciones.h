6bcf6c7c113c8336581027d80cb76cfdff37265d
Lucio alumno (thefenix) <111651430+elfenix-67@users.noreply.github.com>
2024-12-02 11:28:27 -0500

    Update funciones.h

--- nueva version/funciones.h
+++ nueva version/funciones.h
@@ -1,13 +1,14 @@
-﻿#ifndef FUNCIONES_H
+#ifndef FUNCIONES_H
 #define FUNCIONES_H
-
 #define MAX_PRODUCTOS 100
 #define MAX_NOMBRE 50
-
 void iniciar(char nombres[MAX_PRODUCTOS][MAX_NOMBRE], int cantidades[MAX_PRODUCTOS], float recursos[MAX_PRODUCTOS], float tiempos[MAX_PRODUCTOS], int *totalProductos);
 void leer_cadena(char *cadena, int max_len);
 void ingresar_producto(char nombres[MAX_PRODUCTOS][MAX_NOMBRE], int cantidades[MAX_PRODUCTOS], float recursos[MAX_PRODUCTOS], float tiempos[MAX_PRODUCTOS], int *totalProductos);
 void mostrar_productos(char nombres[MAX_PRODUCTOS][MAX_NOMBRE], int cantidades[MAX_PRODUCTOS], float recursos[MAX_PRODUCTOS], float tiempos[MAX_PRODUCTOS], int totalProductos);
+void ordenar_productos_alfabetico(char nombres[MAX_PRODUCTOS][MAX_NOMBRE], int cantidades[MAX_PRODUCTOS], float recursos[MAX_PRODUCTOS], float tiempos[MAX_PRODUCTOS], int totalProductos);
+void mostrar_producto_mayor_cantidad(char nombres[MAX_PRODUCTOS][MAX_NOMBRE], int cantidades[MAX_PRODUCTOS], float recursos[MAX_PRODUCTOS], float tiempos[MAX_PRODUCTOS], int totalProductos);
+void ordenar_productos_por_cantidad(char nombres[MAX_PRODUCTOS][MAX_NOMBRE], int cantidades[MAX_PRODUCTOS], float recursos[MAX_PRODUCTOS], float tiempos[MAX_PRODUCTOS], int totalProductos, int orden);
 void calcular_produccion(char nombres[MAX_PRODUCTOS][MAX_NOMBRE], int cantidades[MAX_PRODUCTOS], float recursos[MAX_PRODUCTOS], float tiempos[MAX_PRODUCTOS], int totalProductos, float recursosDisponibles, float tiempoDisponible);
 void editar_producto(char nombres[MAX_PRODUCTOS][MAX_NOMBRE], int cantidades[MAX_PRODUCTOS], float recursos[MAX_PRODUCTOS], float tiempos[MAX_PRODUCTOS], int totalProductos);
 void eliminar_producto(char nombres[MAX_PRODUCTOS][MAX_NOMBRE], int cantidades[MAX_PRODUCTOS], float recursos[MAX_PRODUCTOS], float tiempos[MAX_PRODUCTOS], int *totalProductos);
